c1d8c2752f976cc95e80c934eab18f42abfccdc7
Gauche <fexploding@gmail.com>
2026-05-25 21:10:58 +0800

    Add files via upload

---
+++ src/add.php
@@ -0,0 +1,45 @@
+<?php
+include('db.php');
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="icon" href="home.png">
+</head>
+<style>
+     body {
+        background-color:rgb(234, 203, 255);
+    }
+</style>
+<body>
+    <h1 id="home">InOutKVPJB</h1>
+
+    <div class="menu">
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="add.php">Tambah Rekod</a></li>
+            <li><a href="view.php">View Rekod</a></li>
+        </ul>
+    </div>
+
+    <marquee direction="right" id="grr">SELAMAT DATANG KE SISTEM InOutKVPJB!</marquee>
+    
+    <script>
+     
+    </script>
+    <center>
+        <h1 id="home">Tambah Maklumat</h1>
+    </center>
+
+
+
+</body>
+</html>
